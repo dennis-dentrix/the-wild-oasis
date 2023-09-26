9d4426a1b41cd437e0c158e50beb241424935986
dennis-dentrix <denkyusya@gmail.com>
2023-09-26 10:17:56 +0300

    first commit

---
+++ src/ui/Input.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+const Input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
+  padding: 0.8rem 1.2rem;
+`;
+
+export default Input;
