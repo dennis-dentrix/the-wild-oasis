c6a37a3411b7a343084a4806935ef7e49f4d26b4
dennis-dentrix <denkyusya@gmail.com>
2023-10-17 10:17:56 +0300

    Uploading image

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,12 +1,33 @@
-import supabase from "./Supabase";
+import supabase, { supabaseUrl } from "./Supabase";
 
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }]);
 
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be created");
   }
+
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+  if (storageError) {
+    // Handle error
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error("Cabins image could not be uploaded");
+  } else {
+    // Handle success
+  }
+
   return data;
 }
 
